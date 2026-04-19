a2ff32637b2dfd7038fb3df3dad96c825e7b27c6
aco4 <anthonythegu@gmail.com>
2026-04-19 01:06:32 -0400

    Improve for mobile

--- make.py
+++ make.py
@@ -211,6 +211,7 @@ html = f'''<!DOCTYPE html>
 <html lang="en">
 <head>
   <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Minimum Research Time</title>
   <style>
     body {{
